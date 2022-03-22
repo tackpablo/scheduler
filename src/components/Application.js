aacfb6075eb9ad3b45b56dd8cd9d0362234e529b
Pablo Tack <tackpablo@gmail.com>
2022-03-22 07:53:50 -0700

    add Appointment component and style

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
-import InterviewerList from "./InterviewerList";
 
 const days = [
   {
@@ -20,14 +19,6 @@ const days = [
     name: "Wednesday",
     spots: 0,
   },
-];
-
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" },
 ];
 
 export default function Application() {
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "components/Appointment/styles.scss";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment">
+      {!props.time && "No Appointments"}
+      {props.time && `Appointment at ${props.time}`}
+    </article>
+  );
+}
