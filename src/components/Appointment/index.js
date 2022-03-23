24fe8721d52ec929f5a7f5d8e00aa8951a5a66cd
Pablo Tack <tackpablo@gmail.com>
2022-03-22 20:26:04 -0700

    add appointment display capabilities

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
+  const { time, interview, student, interviewer } = props;
   return (
-    <article className="appointment">
-      {!props.time && "No Appointments"}
-      {props.time && `Appointment at ${props.time}`}
-    </article>
+    <Fragment>
+      <Header time={time} />
+      <article className="appointment">
+        {interview ? (
+          <Show
+            student={interview.student}
+            interviewer={interview.interviewer}
+          />
+        ) : (
+          <Empty />
+        )}
+      </article>
+    </Fragment>
   );
 }
