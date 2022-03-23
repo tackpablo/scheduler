645c506f9ec7148cf71745adb2faaeeb6a4e7dbc
Pablo Tack <tackpablo@gmail.com>
2022-03-22 21:24:46 -0700

    update application with Appointment components made in storybook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,7 +6,7 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const { time, interview, student, interviewer } = props;
+  const { time, interview } = props;
   return (
     <Fragment>
       <Header time={time} />
