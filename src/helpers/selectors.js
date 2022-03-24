a5fe69cb54bca88ab086fb87e341f2784b2eaae7
Pablo Tack <tackpablo@gmail.com>
2022-03-24 09:45:50 -0700

    add helper function to get interviewers for day, update test cases

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -11,6 +11,25 @@ export function getAppointmentsForDay(state, day) {
   return appointmentsArr;
 }
 
+export function getInterviewersForDay(state, day) {
+  const interviewersArr = [];
+
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  state.days.filter((dayItem) => {
+    if (dayItem.name === day) {
+      dayItem.interviewers.filter((interviewer) => {
+        if (interviewer.id === state.interviewers.id) {
+          interviewersArr.push(state.interviewers[interviewer]);
+        }
+      });
+    }
+  });
+  return interviewersArr;
+}
+
 export function getInterview(state, interview) {
   if (!interview) {
     return null;
