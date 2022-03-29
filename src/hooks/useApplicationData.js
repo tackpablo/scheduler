ec35c23a5dbaa5e3d532b14385b9559156f529c6
Pablo Tack <tackpablo@gmail.com>
2022-03-28 21:18:56 -0700

    update useApplicationData hook with useReducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,147 +1,234 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+function reducer(state, action) {
+  const { day, days, appointments, interviewers, id, interview } = action;
+
+  switch (action.type) {
+    case SET_DAY:
+      return { ...state, day };
+    case SET_APPLICATION_DATA:
+      return { ...state, days, appointments, interviewers };
+    case SET_INTERVIEW: {
+      const appointment = {
+        ...state.appointments[id],
+        interview: { ...interview },
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment,
+      };
+
+      function updateSpots(state, appointments) {
+        // make copy of days array
+        const days = state.days.map((day) => {
+          return { ...day };
+        });
+
+        // console.log("DAYS: ", days);
+
+        function findDay(day) {
+          const daysOfWeek = {
+            Monday: 0,
+            Tuesday: 1,
+            Wednesday: 2,
+            Thursday: 3,
+            Friday: 4,
+          };
+          return daysOfWeek[day];
+        }
+        // find the day's index in days array
+        const dayIndex = findDay(state.day);
+        // console.log("STATE.DAY: ", state.day);
+        // console.log("DAYINDEX: ", dayIndex);
+
+        // check for old state
+        const prevState = state.appointments[id].interview;
+        // console.log("prevSTATE: ", prevState);
+        // check for new state
+        const newState = appointments[id].interview;
+        // console.log("newSTATE: ", newState);
+
+        // create - no old state + new state
+        if (!prevState && newState) {
+          days[dayIndex].spots--;
+        }
+        // delete - old state + no new state
+        if (prevState && !newState) {
+          days[dayIndex].spots++;
+        }
+
+        // return days array
+        return days;
+      }
+
+      const days = updateSpots(state, appointments, id);
+
+      return { ...state, appointments, days };
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
 function useApplicationData() {
-  const [state, setState] = useState({
+  // const [state, setState] = useState({
+  //   day: "Monday",
+  //   days: [],
+  //   appointments: {},
+  //   interviewers: {},
+  // });
+
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {},
   });
 
-  const setDay = (day) => setState({ ...state, day });
+  // const setDay = (day) => setState({ ...state, day });
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
 
   function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
+    // const appointment = {
+    //   ...state.appointments[id],
+    //   interview: { ...interview },
+    // };
 
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment,
+    // };
 
-    setState({
-      ...state,
-      appointments,
-    });
+    // setState({
+    //   ...state,
+    //   appointments,
+    // });
 
     // console.log("STATE.DAYS: ", state.days);
     // console.log("DAY: ", state.day);
     // console.log("STATE: ", state);
 
-    function updateSpots(state, appointments) {
-      // make copy of days array
-      const days = state.days.map((day) => {
-        return { ...day };
-      });
+    // function updateSpots(state, appointments) {
+    //   // make copy of days array
+    //   const days = state.days.map((day) => {
+    //     return { ...day };
+    //   });
 
-      // console.log("DAYS: ", days);
+    //   // console.log("DAYS: ", days);
 
-      function findDay(day) {
-        const daysOfWeek = {
-          Monday: 0,
-          Tuesday: 1,
-          Wednesday: 2,
-          Thursday: 3,
-          Friday: 4,
-        };
-        return daysOfWeek[day];
-      }
-      // find the day's index in days array
-      const dayIndex = findDay(state.day);
-      // console.log("STATE.DAY: ", state.day);
-      // console.log("DAYINDEX: ", dayIndex);
+    //   function findDay(day) {
+    //     const daysOfWeek = {
+    //       Monday: 0,
+    //       Tuesday: 1,
+    //       Wednesday: 2,
+    //       Thursday: 3,
+    //       Friday: 4,
+    //     };
+    //     return daysOfWeek[day];
+    //   }
+    //   // find the day's index in days array
+    //   const dayIndex = findDay(state.day);
+    //   // console.log("STATE.DAY: ", state.day);
+    //   // console.log("DAYINDEX: ", dayIndex);
 
-      // check for old state
-      const prevState = state.appointments[id].interview;
-      // console.log("prevSTATE: ", prevState);
-      // check for new state
-      const newState = appointments[id].interview;
-      // console.log("newSTATE: ", newState);
+    //   // check for old state
+    //   const prevState = state.appointments[id].interview;
+    //   // console.log("prevSTATE: ", prevState);
+    //   // check for new state
+    //   const newState = appointments[id].interview;
+    //   // console.log("newSTATE: ", newState);
 
-      // create - no old state + new state
-      if (!prevState && newState) {
-        days[dayIndex].spots--;
-      }
-      // delete - old state + no new state
-      if (prevState && !newState) {
-        days[dayIndex].spots++;
-      }
+    //   // create - no old state + new state
+    //   if (!prevState && newState) {
+    //     days[dayIndex].spots--;
+    //   }
+    //   // delete - old state + no new state
+    //   if (prevState && !newState) {
+    //     days[dayIndex].spots++;
+    //   }
 
-      // return days array
-      return days;
-    }
+    //   // return days array
+    //   return days;
+    // }
 
     return axios.put(`/api/appointments/${id}`, { interview }).then((res) => {
-      const days = updateSpots(state, appointments);
-      setState({ ...state, appointments, days });
+      // const days = updateSpots(state, appointments);
+      // setState({ ...state, appointments, days });
+      dispatch({ type: SET_INTERVIEW, id, interview });
     });
   }
 
   function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
+    // const appointment = {
+    //   ...state.appointments[id],
+    //   interview: null,
+    // };
 
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id]: appointment,
+    // };
 
-    setState({
-      ...state,
-      appointments,
-    });
+    // setState({
+    //   ...state,
+    //   appointments,
+    // });
 
-    function updateSpots(state, appointments) {
-      // make copy of days array
-      const days = state.days.map((day) => {
-        return { ...day };
-      });
+    // function updateSpots(state, appointments) {
+    //   // make copy of days array
+    //   const days = state.days.map((day) => {
+    //     return { ...day };
+    //   });
 
-      // console.log("DAYS: ", days);
+    //   // console.log("DAYS: ", days);
 
-      function findDay(day) {
-        const daysOfWeek = {
-          Monday: 0,
-          Tuesday: 1,
-          Wednesday: 2,
-          Thursday: 3,
-          Friday: 4,
-        };
-        return daysOfWeek[day];
-      }
-      // find the day's index in days array
-      const dayIndex = findDay(state.day);
-      // console.log("STATE.DAY: ", state.day);
-      // console.log("DAYINDEX: ", dayIndex);
+    //   function findDay(day) {
+    //     const daysOfWeek = {
+    //       Monday: 0,
+    //       Tuesday: 1,
+    //       Wednesday: 2,
+    //       Thursday: 3,
+    //       Friday: 4,
+    //     };
+    //     return daysOfWeek[day];
+    //   }
+    //   // find the day's index in days array
+    //   const dayIndex = findDay(state.day);
+    //   // console.log("STATE.DAY: ", state.day);
+    //   // console.log("DAYINDEX: ", dayIndex);
 
-      // check for old state
-      const prevState = state.appointments[id].interview;
-      // console.log("prevSTATE: ", prevState);
-      // check for new state
-      const newState = appointments[id].interview;
-      // console.log("newSTATE: ", newState);
+    //   // check for old state
+    //   const prevState = state.appointments[id].interview;
+    //   // console.log("prevSTATE: ", prevState);
+    //   // check for new state
+    //   const newState = appointments[id].interview;
+    //   // console.log("newSTATE: ", newState);
 
-      // create - no old state + new state
-      if (!prevState && newState) {
-        days[dayIndex].spots--;
-      }
-      // delete - old state + no new state
-      if (prevState && !newState) {
-        days[dayIndex].spots++;
-      }
+    //   // create - no old state + new state
+    //   if (!prevState && newState) {
+    //     days[dayIndex].spots--;
+    //   }
+    //   // delete - old state + no new state
+    //   if (prevState && !newState) {
+    //     days[dayIndex].spots++;
+    //   }
 
-      // return days array
-      return days;
-    }
+    //   // return days array
+    //   return days;
+    // }
 
     return axios.delete(`/api/appointments/${id}`).then((res) => {
-      const days = updateSpots(state, appointments);
-      setState({ ...state, appointments, days });
+      // const days = updateSpots(state, appointments);
+      // setState({ ...state, appointments, days });
+      dispatch({ type: SET_INTERVIEW, id, interview: null });
     });
   }
 
@@ -151,12 +238,18 @@ function useApplicationData() {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers"),
     ]).then((all) => {
-      setState((prev) => ({
-        ...prev,
+      // setState((prev) => ({
+      //   ...prev,
+      //   days: all[0].data,
+      //   appointments: all[1].data,
+      //   interviewers: all[2].data,
+      // }));
+      dispatch({
+        type: SET_APPLICATION_DATA,
         days: all[0].data,
         appointments: all[1].data,
         interviewers: all[2].data,
-      }));
+      });
     });
   }, []);
 
