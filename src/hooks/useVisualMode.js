0b56a46113e78bf5ae8af853d003b3c3d0fd1a4a
Pablo Tack <tackpablo@gmail.com>
2022-03-30 08:02:13 -0700

    refactor useVisualMode hook to remove unnecessary state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,31 +1,47 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+  // const [mode, setMode] = useState(initial);
+  // const [history, setHistory] = useState([initial]);
+
+  // function transition(newMode, replace = false) {
+  //   if (replace) {
+  //     setHistory((prev) => [...prev.slice(0, -1), newMode]);
+  //   } else {
+  //     setHistory((prev) => [...prev, newMode]);
+  //   }
+
+  //   setMode(newMode);
+  // }
+
+  // function back() {
+  //   setHistory((prev) => {
+  //     if (history.length === 1) {
+  //       return [...history];
+  //     }
+
+  //     const reducedMode = [...prev.slice(0, -1)];
+  //     setMode(reducedMode[reducedMode.length - 1]);
+
+  //     return reducedMode;
+  //   });
+  // }
+
   const [history, setHistory] = useState([initial]);
-
-  function transition(newMode, replace = false) {
-    if (replace) {
-      setHistory((prev) => [...prev.slice(0, -1), newMode]);
-    } else {
-      setHistory((prev) => [...prev, newMode]);
-    }
-
-    setMode(newMode);
-  }
-
-  function back() {
+  const transition = (newMode, replace = false) => {
     setHistory((prev) => {
-      if (prev.length === 1) {
-        return [...prev];
-      }
-
-      const reducedMode = [...prev.slice(0, -1)];
-      setMode(reducedMode[reducedMode.length - 1]);
-
-      return reducedMode;
+      if (replace) return [newMode, ...prev.slice(1)];
+      return [newMode, ...prev];
     });
-  }
+  };
+
+  const back = () => {
+    setHistory((prev) => {
+      if (prev.length <= 1) return prev;
+      return prev.slice(1);
+    });
+  };
+  const mode = history[0];
 
   return { mode, transition, back };
 }
